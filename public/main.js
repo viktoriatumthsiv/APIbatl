7b464a908fe3a5ce7d41cdd2030c986f8ed9ffcc
Viktoria Tumthsiv <viktoriatumthisv@gmail.com>
2026-01-28 19:41:33 +0200

    Додала кнопку видалення

--- public/main.js
+++ public/main.js
@@ -1,15 +1,22 @@
+
 async function load() {
     const r = await fetch('/dates');
     const data = await r.json();
     document.getElementById('list').innerHTML = data.map(d => `
         <div class="date-card">
             <b>${d.date}</b>: ${d.title} <br>
-            <small> ${d.funFact}</small>
+            <small>✨ ${d.funFact}</small>
+            <button class="delete-btn" onclick="deleteDate('${d.id}')">Видалити</button>
         </div>
     `).join('');
 }
 
-
+async function deleteDate(id) {
+    if (confirm("Видалити цю подію?")) {
+        await fetch(`/dates/${id}`, { method: 'DELETE' });
+        load(); // Оновлюємо список після видалення
+    }
+}
 async function addDate() {
     const body = {
         title: document.getElementById('title').value,
